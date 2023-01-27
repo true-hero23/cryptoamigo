b0c4d1b35d1e7f454646e3a6ff40d9c590434ddb
kwstaspexli <kostaspechli2@gmail.com>
2023-01-28 00:06:42 +0200

    New Update

--- src/components/TradingPage/TradingDatagrid.js
+++ src/components/TradingPage/TradingDatagrid.js
@@ -396,7 +396,7 @@ function TradingDatagrid() {
               return "";
             }}
             onCellClick={(params) => {
-              params.field !== "Watchlist" && navigate(`/${params.id}`);
+              params.field !== "Watchlist" && navigate(`/coins/${params.id}`);
             }}
           />
         </Box>
